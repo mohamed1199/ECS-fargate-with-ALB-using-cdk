6bd687aed0b2786f294c953d58545765637b7679
mohamed nebbai <nebbai1999@gmail.com>
2023-03-17 11:50:10 +0100

    version 1

--- bin/cdk-entry.ts
+++ bin/cdk-entry.ts
@@ -1,21 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkEntryStack } from '../lib/cdk-entry-stack';
+import { CdkWorkshopStack } from '../lib/cdk-entry-stack';
 
 const app = new cdk.App();
-new CdkEntryStack(app, 'CdkEntryStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new CdkWorkshopStack(app, 'CdkEntryStack', {
+  env: {
+    account: "add your account id here",
+    region: "add your region here",
+  },
 });
--- lib/cdk-entry-stack.ts
+++ lib/cdk-entry-stack.ts
@@ -1,16 +1,100 @@
 import * as cdk from 'aws-cdk-lib';
+import { IpAddresses, Peer, Port, SecurityGroup, Subnet, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { AppProtocol, Cluster, ContainerImage, FargateService, FargateTaskDefinition, ListenerConfig, Protocol, TaskDefinition } from 'aws-cdk-lib/aws-ecs';
+import { ApplicationListener, ApplicationLoadBalancer, ApplicationProtocol, ListenerAction, ListenerCondition } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
-export class CdkEntryStack extends cdk.Stack {
+
+export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // get a VPC by ID
+    const myvpc = Vpc.fromLookup(this, "vpc-0803355579cde85c4", {
+      vpcId: "vpc-0803355579cde85c4",
+      isDefault: true
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkEntryQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    // create a custom VPC
+    /* const myvpc = new Vpc(this, 'vpc101', {
+      ipAddresses: IpAddresses.cidr("172.20.0.0/16"),
+      vpcName: "vpc_cdk",
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "public-subnet",
+          subnetType: SubnetType.PUBLIC,
+        },
+      ],
+      maxAzs: 2
+    });
+ */
+    // create a cluster
+    const myCluster = new Cluster(this, "ClusterCDK", {
+      clusterName: "ClusterCDK",
+      vpc: myvpc,
+    });
+
+    //create a task definition
+    const taskDef = new FargateTaskDefinition(this, "MicroOneTaskDef", {
+      cpu: 256,
+      memoryLimitMiB: 512,
+    });
+
+    const containerDef = taskDef.addContainer("micro1", {
+      image: ContainerImage.fromRegistry("mohamednebbai/micro1", {
+      }),
+      containerName: "micro1",
+      portMappings: [
+        {
+          containerPort: 80,
+          protocol: Protocol.TCP,
+        }
+      ]
+    });
+
+    //create a security group for service
+    const serviceSG = new SecurityGroup(this, 'FargateServiceSG', {
+      vpc: myvpc,
+      securityGroupName: "FargateServiceSG"
+    });
+    serviceSG.addIngressRule(Peer.anyIpv4(), Port.tcp(80));
+    serviceSG.addEgressRule(Peer.anyIpv4(), Port.allTraffic());
+
+    // create a fargate service
+    const fargateService = new FargateService(this, "service101", {
+      cluster: myCluster,
+      taskDefinition: taskDef,
+      desiredCount: 1,
+      serviceName: "Micro1Service",
+      assignPublicIp: true,
+      securityGroups: [serviceSG]
+    });
+
+
+    // create an internet (public) alb
+    const Alb = new ApplicationLoadBalancer(this, 'PublicAlb', {
+      vpc: myvpc,
+      internetFacing: true,
+      securityGroup: serviceSG,
+    });
+
+    const listener = new ApplicationListener(this, 'Listener', {
+      loadBalancer: Alb,
+      port: 80,
+    });
+
+    fargateService.registerLoadBalancerTargets(
+      {
+        containerName: containerDef.containerName,
+        containerPort: 80,
+        newTargetGroupId: 'target group 1',
+        listener: ListenerConfig.applicationListener(listener),
+      },
+    );
   }
 }
+
+
+
